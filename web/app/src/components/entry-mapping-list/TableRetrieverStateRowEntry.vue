4cbcbbc88be6381df8c0c6a762ed95d197a3101b
almanac1631 <169612107+almanac1631@users.noreply.github.com>
2025-01-20 23:26:27 +0100

    add retriever grouping and restructure icon folders

---
+++ web/app/src/components/entry-mapping-list/TableRetrieverStateRowEntry.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+const props = defineProps({
+  present: Boolean
+});
+
+const presentText = computed(() => props.present ? "✅" : "❌");
+</script>
+
+<template>
+  <td class="p-3 text-center">
+    {{ presentText }}
+  </td>
+</template>
