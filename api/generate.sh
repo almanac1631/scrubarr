79408e991aef2f86eab8414534304ca3e705f403
almanac1631 <169612107+almanac1631@users.noreply.github.com>
2025-02-09 22:46:55 +0100

    add code generation tools/scripts

---
+++ api/generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+openapi-generator generate -i ./openapi.yaml -g openapi -o .
+oapi-codegen -config ./oapi-codegen-config.yaml ./openapi.json
+openapi-generator generate -i ./openapi.yaml -g typescript-axios -o ../web/app/src/api/
