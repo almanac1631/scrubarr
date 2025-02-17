2d2ddab9be610f29c95ca095f5840bc8eef17e7f
almanac1631 <169612107+almanac1631@users.noreply.github.com>
2025-02-17 20:26:14 +0100

    add missing gitleaks allow

--- web/app/src/auth/auth.spec.ts
+++ web/app/src/auth/auth.spec.ts
@@ -8,11 +8,11 @@ describe("check token validity using claims", () => {
             exp: Math.floor(Date.now() / 1000) + 3600,
         };
         const encodedPayload = btoa(JSON.stringify(payload));
-        let jwtStr = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.${encodedPayload}.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`;
+        let jwtStr = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.${encodedPayload}.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`;//gitleaks:allow
         expect(isTokenStillValidClaimsBased(jwtStr)).toBe(true);
     });
     test("checks expired token claims", async () => {
-        let jwtStr = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzk3MTI0MzMsImV4cCI6MTczOTc1MjQzM30.1LOyxfn7rwwFyrKeVWnAVSt9GOohlZvMjUpykXGpzpI`;
+        let jwtStr = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzk3MTI0MzMsImV4cCI6MTczOTc1MjQzM30.1LOyxfn7rwwFyrKeVWnAVSt9GOohlZvMjUpykXGpzpI`; //gitleaks:allow
         expect(isTokenStillValidClaimsBased(jwtStr)).toBe(false);
     });
     test("checks malformed token", async () => {
