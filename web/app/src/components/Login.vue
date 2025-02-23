e2f8c466267b58dea16cf3b05d6bb30deaceb334
almanac1631 <169612107+almanac1631@users.noreply.github.com>
2025-02-23 23:33:10 +0100

    Set autocomplete attributes

--- web/app/src/components/Login.vue
+++ web/app/src/components/Login.vue
@@ -45,7 +45,7 @@ async function submitLoginForm(e: any) {
           <div>
             <label for="username" class="block text-sm/6 font-medium text-gray-100">Username</label>
             <div class="mt-2">
-              <input type="text" name="username" id="username" autocomplete="on" required v-model="username"
+              <input type="text" name="username" id="username" autocomplete="username" required v-model="username"
                      class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-500 sm:text-sm/6">
             </div>
           </div>
@@ -55,7 +55,7 @@ async function submitLoginForm(e: any) {
               <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
             </div>
             <div class="mt-2">
-              <input type="password" name="password" id="password" autocomplete="on" required v-model="password"
+              <input type="password" name="password" id="password" autocomplete="current-password" required v-model="password"
                      class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-500 sm:text-sm/6">
             </div>
           </div>
